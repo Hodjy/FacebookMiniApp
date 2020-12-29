652ed38d8888e19776574fd5f99bd034119f29bc
Hodjy <62711261+Hodjy@users.noreply.github.com>
2020-12-29 16:36:05 +0200

    Name changes
    Project folder and name.

---
+++ A21 Ex01 Hod 204479745 Matan 312539539/FacebookView/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace A21_Ex01_Hod_204479745_Matan_312539539
+{
+    public static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        public static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainFacebookForm());
+        }
+    }
+}
